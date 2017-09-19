471fa8c59b2c2e3986956e74bb2e544ad592f071
Sean Auriti <sean@alphaonelabs.com>
2017-09-18 21:47:51 -0400

    signup is working

--- Pages/SignupPage.js
+++ Pages/SignupPage.js
@@ -3,6 +3,65 @@ function back() {
 	console.log('back');
 }
 
+var Observable = require("FuseJS/Observable");
+
+var password = Observable("");
+var username = Observable("");
+var email = Observable("");
+var first_name = Observable("");
+var last_name = Observable("");
+var error = Observable("");
+
+
+function formEncode(obj) {
+    var str = [];
+    for (var p in obj)
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    return str.join("&");
+}
+
 module.exports = {
-	back: back
+    password: password,
+    username: username,
+    email: email,
+    last_name: last_name,
+    first_name: first_name,
+    error: error,
+    back: back,
+    doSignup: function doSignup(e) {
+        var requestObject = { 
+        	username: username.value, 
+        	password: password.value,
+        	email: email.value,
+        	first_name: first_name.value,
+        	last_name: last_name.value 
+        };
+        url = 'http://127.0.0.1:8000/api/signup/';
+        //url = 'https://emrals-staging.herokuapp.com/api/login/';
+        fetch(url, {
+            method: 'POST',
+            headers: { "Content-type": "application/x-www-form-urlencoded" },
+            body: formEncode(requestObject)
+        }).then(function(response) {
+            status = response.status;
+            response_ok = response.ok;
+            return response.json();
+        }).then(function(responseObject) {
+
+            if (responseObject) {
+                if (responseObject.token) {
+                    console.log(responseObject.token);
+                    router.goto("splash");
+
+                } else {
+                    error.value = responseObject.error;
+                }
+            }
+
+        }).catch(function(err) {
+
+            console.log("Fetch error: " + err);
+        });
+
+    }
 };
