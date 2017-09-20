6d45bd2ff219fcea4d01934d8cc87a2edd4a8969
mareiska <mareiska@alphaonelabs.com>
2017-09-20 19:02:36 -0400

    Adding Impact Page

--- Pages/SplashPage.js
+++ Pages/SplashPage.js
@@ -1,5 +1,7 @@
 module.exports = {
 	goToEcans: function goToEcans() { router.goto("ecans"); },
 	goToAlerts: function goToAlerts() { router.goto("alerts"); },
-	goToLogin: function goToLogin() { router.goto("login"); }
+	goToLogin: function goToLogin() { router.goto("login"); },
+	goToImpact: function goToImpact() { router.goto("impact"); }
+
 };
