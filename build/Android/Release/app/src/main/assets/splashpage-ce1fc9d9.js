e959fa2ed4d355c3a2b28660177ec18b669e1b3c
Sean Auriti <sean@alphaonelabs.com>
2017-09-29 19:48:40 -0400

    Battery level and time are missing on top bar closes #42

--- build/Android/Release/app/src/main/assets/splashpage-ce1fc9d9.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	goToEcans: function goToEcans() { router.push("ecans"); },
-	goToAlerts: function goToAlerts() { router.push("alerts"); },
-	goToLogin: function goToLogin() { router.push("login"); },
-	goToImpact: function goToImpact() { router.push("impact"); }
-};
