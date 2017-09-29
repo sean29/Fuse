3320f9001832598f095ecfcbcb91a65c3cdc527e
Sean Auriti <sean@alphaonelabs.com>
2017-09-29 01:11:34 -0400

    ready for deploy

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -2,4 +2,4 @@ curl --remote-name https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_1_2_1
 echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target /
 fuse build --target=iOS --configuration=Release || true
 fuse build --target=Android --configuration=Release || true
-sed -i -e 's/pk_test/$STRIPE_PRIVATE_KEY/g' MainView.js
+sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
