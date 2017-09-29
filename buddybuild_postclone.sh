bce141c9c5bad693408c1d5324e63c299da38f81
Sean Auriti <sean@alphaonelabs.com>
2017-09-29 01:22:26 -0400

    put sed replace before

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,5 +1,6 @@
 curl --remote-name https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_1_2_1_13974.pkg
 echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target /
+sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
+cat MainView.js
 fuse build --target=iOS --configuration=Release || true
 fuse build --target=Android --configuration=Release || true
-sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
