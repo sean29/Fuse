11ed9befd0e7f9b01809cd8229624510af5f88fa
mareiska <mareiska@alphaonelabs.com>
2017-09-28 21:52:56 -0400

    Impact Thank You Page

---
+++ Pages/ImpactThankYouPage.js
@@ -0,0 +1,8 @@
+var Observable = require("FuseJS/Observable");
+
+module.exports = {
+	goToEcans: function goToEcans() { router.push("ecans"); },
+	goToAlerts: function goToAlerts() { router.push("alerts"); },
+	goToImpact: function goToImpact() { router.push("impact"); }
+
+};
