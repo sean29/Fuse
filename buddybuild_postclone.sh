c293a450f922c09ea001755aa21d4a5e1c766826
Sean Auriti <seanauriti@users.noreply.github.com>
2017-10-03 04:26:40 -0400

    update

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -3,6 +3,5 @@ echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target /
 sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
 sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" build/Android/Release/app/src/main/assets/mainview-cb8c4a65.js
 npm install -g fusepm
-rm -rf fuse_modules
 fusepm install https://github.com/bolav/fuse-cocoapods
 fuse build --target=iOS -DCOCOAPODS --configuration=Release || true
