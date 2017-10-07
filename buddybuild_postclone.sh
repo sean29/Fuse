5d2f269854cdf13f0622a08caaabb22aaa81abb9
Sean Auriti <sean@alphaonelabs.com>
2017-10-06 21:21:34 -0400

    tweaking build  procedure

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,12 +1,17 @@
 curl --remote-name https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_1_2_1_13974.pkg
 echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target /
 sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
-sed -i -e "s/APPID_HERE/${FB_APP_ID}/g" emrals.unoproj
 npm install -g fusepm
 fusepm install
 
 fuse build --target=iOS -DCOCOAPODS --configuration=Release || true
 
 
-build/ios/release 
+/usr/libexec/PlistBuddy -x -c "Set :FacebookAppID $FB_APP_ID" build/Release/iOS/src/Targets/iOS/Uno-iOS/emrals-Info.plist
+/usr/libexec/PlistBuddy -x -c "Set :FacebookDisplayName Emrals" build/Release/iOS/src/Targets/iOS/Uno-iOS/emrals-Info.plist
+/usr/libexec/PlistBuddy -x -c "Set :CFBundleURLTypes:0:CFBundleURLSchemes:0 fb$FB_APP_ID" build/Release/iOS/src/Targets/iOS/Uno-iOS/emrals-Info.plist
+
+# copy the Context.mm file that initalizes buddybuild and facebook
+cp Context.mm build/Release/iOS/src/Targets/iOS/Uno-iOS/
+
 
