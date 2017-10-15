fb6063996ba6a6033b4feaa65c402cfa1f66d244
Sean Auriti <sean@alphaonelabs.com>
2017-10-15 15:23:19 -0400

    update

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,7 +1,5 @@
-OUTPUT2="$(uname -a)"
-curl -X POST --data-urlencode "payload={\"channel\": \"#buddybuild-android\", \"username\": \"webhookbot\", \"text\": \"uname: ${OUTPUT2}\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
-
-
+OUTPUT="$(uname -a)"
+curl -X POST --data-urlencode "payload={\"channel\": \"#buddybuild-android\", \"username\": \"webhookbot\", \"text\": \"uname: ${OUTPUT}\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
 
 curl --remote-name http://dl.google.com/android/repository/android-ndk-r15c-darwin-x86_64.zip
 unzip android-ndk-r15c-darwin-x86_64.zip | grep -v inflating: | grep -v creating: | grep -v extracting:
@@ -9,36 +7,27 @@ rm android-ndk-r15c-darwin-x86_64.zip
 mkdir -p "/Users/buddybuild/Library/Android/sdk/"
 rm -rf /Users/buddybuild/Library/Android/sdk/ndk-bundle
 mv android-ndk-r15c "/Users/buddybuild/Library/Android/sdk/ndk-bundle"
-echo "Installed NDK to /Users/buddybuild/Library/Android/sdk/ndk-bundle"
-
 
 curl --remote-name https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_1_2_1_13974.pkg
 echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target / || true
 
-OUTPUT="$(fuse --version)"
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#buddybuild-android\", \"username\": \"webhookbot\", \"text\": \"fuse --version: ${OUTPUT} \", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+echo "android 13"
 
 
-echo "android 12"
 APP_VERSION=0.0.4
 
 
 sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
 sed -i -e "s/slack_key/${SLACK_KEY_VAR}/g" MainView.js
 sed -i -e "s/BUDDYBUILD_BUILD_NUMBER/${BUDDYBUILD_BUILD_NUMBER}/g" MainView.js
-sed -i -e "s/FacebookAppIDREPLACE/${FB_APP_ID}/g" temp.plist
-sed -i -e "s/APP_VERSION/${APP_VERSION}/g" temp.plist
 sed -i -e "s/APP_VERSION/${APP_VERSION}/g" MainView.js
 sed -i -e "s/POST_SERVER_ITEM_ACCESS_TOKEN/${POST_SERVER_ITEM_ACCESS_TOKEN}/g" MainView.js
-
 
 npm install -g fusepm
 fusepm install
 
-#fuse install android
-
-OUTPUT="$((fuse install android < ./sdkinstall.txt || true) 2>&1)"
+OUTPUT="$((fuse install android < ./sdkinstall.txt |head || true) 2>&1)"
 
 
 curl -X POST --data-urlencode "payload={\"channel\": \"#buddybuild-android\", \"username\": \"webhookbot\", \"text\": \"fuse install android: ${OUTPUT} \", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
