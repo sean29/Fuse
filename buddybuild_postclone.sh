4778b5e9bcb69c7d61e32a83845378ffe23f9dc3
Sean Auriti <sean@alphaonelabs.com>
2017-10-14 21:44:16 -0400

    testing buddybuild for Android

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,8 +1,11 @@
-curl --remote-name https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_1_2_1_13974.pkg
-echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target /
+if [ -e "emrals.xcodeproj" ]; then
+	curl --remote-name https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_1_2_1_13974.pkg
+	echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target /
+	npm install -g fusepm
+	fusepm install
+fi
 
-APP_VERSION=0.0.3
-
+APP_VERSION=0.0.4
 
 sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
 sed -i -e "s/slack_key/${SLACK_KEY_VAR}/g" MainView.js
@@ -13,16 +16,13 @@ sed -i -e "s/APP_VERSION/${APP_VERSION}/g" MainView.js
 sed -i -e "s/POST_SERVER_ITEM_ACCESS_TOKEN/${POST_SERVER_ITEM_ACCESS_TOKEN}/g" MainView.js
 
 
-npm install -g fusepm
-fusepm install
-
 if [ -n "${FB_APP_ID}" ]; then             
   fuse build --target=iOS -DCOCOAPODS --configuration=Release || true
   cp temp.plist build/iOS/Release/emrals/emrals-Info.plist 
   cp Context.mm build/iOS/Release/src/Targets/iOS/Uno-iOS/
 else
-  #rm -rf emrals.xcodeproj
+  rm -rf emrals.xcodeproj
   #cp build.temp build.gradle 
-  fuse install android
+  fuse install android || true
   fuse build --target=Android --configuration=Release || true
 fi
