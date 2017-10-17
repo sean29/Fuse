c655ae728d2d3ae16ec46933fcde3361613e55f2
Sean Auriti <sean@alphaonelabs.com>
2017-10-16 22:04:25 -0400

    fixes buddybuild

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,26 +1,21 @@
 APP_VERSION=0.0.4
 
-uname -a | grep 'Buddybuilds' &> /dev/null
-if [ $? == 0 ]; then
 
-	curl --remote-name https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_1_2_1_13974.pkg
-	echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target /
+curl --remote-name https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_1_2_1_13974.pkg
+echo password | sudo installer -pkg fuse_osx_1_2_1_13974.pkg -target /
 
-	sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
-	sed -i -e "s/slack_key/${SLACK_KEY_VAR}/g" MainView.js
-	sed -i -e "s/BUDDYBUILD_BUILD_NUMBER/${BUDDYBUILD_BUILD_NUMBER}/g" MainView.js
-	sed -i -e "s/FacebookAppIDREPLACE/${FB_APP_ID}/g" temp.plist
-	sed -i -e "s/APP_VERSION/${APP_VERSION}/g" temp.plist
-	sed -i -e "s/APP_VERSION/${APP_VERSION}/g" MainView.js
-	sed -i -e "s/POST_SERVER_ITEM_ACCESS_TOKEN/${POST_SERVER_ITEM_ACCESS_TOKEN}/g" MainView.js
+sed -i -e "s/pk_test/${STRIPE_PRIVATE_KEY}/g" MainView.js
+sed -i -e "s/slack_key/${SLACK_KEY_VAR}/g" MainView.js
+sed -i -e "s/BUDDYBUILD_BUILD_NUMBER/${BUDDYBUILD_BUILD_NUMBER}/g" MainView.js
+sed -i -e "s/FacebookAppIDREPLACE/${FB_APP_ID}/g" temp.plist
+sed -i -e "s/APP_VERSION/${APP_VERSION}/g" temp.plist
+sed -i -e "s/APP_VERSION/${APP_VERSION}/g" MainView.js
+sed -i -e "s/POST_SERVER_ITEM_ACCESS_TOKEN/${POST_SERVER_ITEM_ACCESS_TOKEN}/g" MainView.js
 
-	npm install -g fusepm
-	fusepm install
+npm install -g fusepm
+fusepm install
 
-	         
-	fuse build --target=iOS -DCOCOAPODS --configuration=Release || true
-	cp temp.plist build/iOS/Release/emrals/emrals-Info.plist 
-	cp Context.mm build/iOS/Release/src/Targets/iOS/Uno-iOS/
-
-fi
-
+         
+fuse build --target=iOS -DCOCOAPODS --configuration=Release || true
+cp temp.plist build/iOS/Release/emrals/emrals-Info.plist 
+cp Context.mm build/iOS/Release/src/Targets/iOS/Uno-iOS/
